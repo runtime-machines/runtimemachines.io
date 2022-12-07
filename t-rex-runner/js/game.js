ef645e5fa78c563b1a703d80783fc6194d5476f7
Paolo Bonomi <paolobonomi@mbp2020.local>
2022-12-07 15:58:53 +0100

    adds green button and white clounds to sprite-2x

--- t-rex-runner/js/game.js
+++ t-rex-runner/js/game.js
@@ -154,26 +154,26 @@ Runner.classes = {
  * Sprite definition layout of the spritesheet.
  * @enum {Object}
  */
-Runner.spriteDefinition = {
+ Runner.spriteDefinition = {
   LDPI: {
-    CACTUS_LARGE: {x: 332, y: 2},
-    CACTUS_SMALL: {x: 228, y: 2},
-    CLOUD: {x: 86, y: 2},
-    HORIZON: {x: 2, y: 54},
-    PTERODACTYL: {x: 134, y: 2},
-    RESTART: {x: 2, y: 2},
-    TEXT_SPRITE: {x: 484, y: 2},
-    TREX: {x: 677, y: 2}
+      CACTUS_LARGE:{x:332,y:2},
+      CACTUS_SMALL:{x:228,y:2},
+      CLOUD:{x:86,y:2},
+      HORIZON:{x:2,y:54},
+      PTERODACTYL:{x:134,y:2},
+      RESTART:{x:2,y:2},
+      TEXT_SPRITE:{x:484,y:2},
+      TREX:{x:677,y:2}
   },
   HDPI: {
-    CACTUS_LARGE: {x: 652,y: 2},
-    CACTUS_SMALL: {x: 446,y: 2},
-    CLOUD: {x: 166,y: 2},
-    HORIZON: {x: 2,y: 104},
-    PTERODACTYL: {x: 260,y: 2},
-    RESTART: {x: 2,y: 2},
-    TEXT_SPRITE: {x: 954,y: 2},
-    TREX: {x: 1338,y: 2}
+      CACTUS_LARGE:{x:652,y:2},
+      CACTUS_SMALL:{x:446,y:2},
+      CLOUD:{x:166,y:2},
+      HORIZON:{x:2,y:104},
+      PTERODACTYL:{x:260,y:2},
+      RESTART:{x:2,y:2},
+      TEXT_SPRITE:{x:954,y:2},
+      TREX:{x:1338,y:2}
   }
 };
 
