b612890ea65b470c50653ea5d61661d8b2afdf5f
Paolo Bonomi <paolobonomi@mbp2020.Home>
2022-12-10 21:01:48 +0100

    refactors all legacy code

---
+++ t-rex-runner/js/cfg.js
@@ -0,0 +1,24 @@
+/**
+ * Default game width.
+ * @const
+ */
+ var DEFAULT_WIDTH = 1200;
+
+ /**
+  * Frames per second.
+  * @const
+  */
+ var FPS = 60;
+ 
+ /** @const */
+ var IS_HIDPI = window.devicePixelRatio > 1;
+ 
+ /** @const */
+ var IS_IOS = window.navigator.userAgent.indexOf('CriOS') > -1 ||
+     window.navigator.userAgent == 'UIWebViewForStaticFileContent';
+ 
+ /** @const */
+ var IS_MOBILE = window.navigator.userAgent.indexOf('Mobi') > -1 || IS_IOS;
+ 
+ /** @const */
+ var IS_TOUCH_ENABLED = 'ontouchstart' in window;
