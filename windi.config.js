4a6295d05a900c45117b19d17dd85eccd33bbd1f
Elia Maggioni <elia.maggioni@gmail.com>
2022-12-14 14:52:19 +0100

    reworked grid layout, added images, changed form

--- windi.config.js
+++ windi.config.js
@@ -17,6 +17,34 @@ export default defineConfig({
 				...theme('colors'),
 				orange: '#FF8855',
 			}),
+			animation: {
+				wiggle: 'wiggle 1s ease-in-out',
+				wigglechild: 'wigglechild 1s ease-in-out',
+				wigglemiddle: 'wigglemiddle 1s ease-in-out',
+			},
+			keyframes: {
+				wiggle: {
+					'0%, 100%': { 'margin-left': '50%' },
+					'60%': { 'margin-left': '50.4%' },
+					'80%': { 'margin-left': '49.6%' },
+					'20%': { 'margin-left': '50.8%' },
+					'40%': { 'margin-left': '49.2%' },
+				},
+				wigglechild: {
+					'0%, 100%': { left: '-50%' },
+					'60%': { left: '-50.4%' },
+					'80%': { left: '-49.6%' },
+					'20%': { left: '-50.8%' },
+					'40%': { left: '-49.2%' },
+				},
+				wigglemiddle: {
+					'0%, 100%': { left: '50%' },
+					'60%': { left: '50.4%' },
+					'80%': { left: '49.6%' },
+					'20%': { left: '50.8%' },
+					'40%': { left: '49.2%' },
+				},
+			},
 		},
 	},
 	extract: {
