82f887c91b5a06b2eec99be5146290c114d1801c
Paolo Bonomi <paolo.bonomi@outlook.com>
2022-12-28 10:38:19 +0100

    changes some riddle cfgs

--- public/trex-runner/js/riddle.js
+++ public/trex-runner/js/riddle.js
@@ -5,12 +5,15 @@ Riddle.MAX_DISTANCE = 500;
 Riddle.PICKUP_TO_COLLECT = 5;
 Riddle.USE_PICKUPS = false;
 Riddle.MIN_DEATHS = 2;
-Riddle.MAX_OBSTACLES = 1;
+Riddle.MAX_OBSTACLES = 8;
 Riddle.SHOW_OBSTACLES_LEFT = true;
 Riddle.RESET_OBSTACLES_LEFT_UPON_DEATH = false;
 Riddle.DEBUG_COLLIDER = false;
 Riddle.CONFETTI = false;
 Riddle.MODAL = false;
+
+//debug
+localStorage.setItem('websiteState', 'playful');
 
 /**
  *
