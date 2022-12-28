da1b13e2dbd45b7a1077685c3e4e9e75b5304ac5
Elia Maggioni <elia.maggioni@gmail.com>
2022-12-28 13:51:35 +0100

    slider modified, some graphical fixes done

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,3 +8,21 @@ export enum WebsiteState {
 export function getSides() {
 	return ['left', 'right'];
 }
+
+export function typeWriter(arrayDiv: HTMLElement[], texts: string[], timeSpan: number, condition: () => boolean) {
+	arrayDiv.forEach(function (element: HTMLElement) {
+		element.textContent = '';
+	});
+	let time = 20;
+
+	for (let index = 0; index < arrayDiv.length; index++) {
+		const div = arrayDiv[index];
+		for (let e = 0; e < texts[index].length; e++) {
+			const char = texts[index][e];
+			setTimeout(() => {
+				if (condition()) div.innerHTML += char;
+			}, time);
+			time += timeSpan;
+		}
+	}
+}
